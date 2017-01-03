b7a58188185b5ecdcd777c9f9cc7adf035ed90fc
Claude BUENO <claude.bueno@outlook.com>
2017-01-03 15:21:35 +0100

    TableauPersonne-OK

---
+++ Workspace_m2i_jsf/FichierTxt/src/com/formation/UploadFichier.java
@@ -0,0 +1,10 @@
+package com.formation;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "uploadfichier", eager = true)
+@SessionScoped
+public class UploadFichier {
+
+}
